8f25efe236cdf1f5732c1962d10ddd086e21857e
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-22 21:17:58 -0300

    deleted memcpy

--- get_next_line.h
+++ get_next_line.h
@@ -10,7 +10,6 @@ char			*ft_strdup(const char *s1);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strchr(const char *s, int c);
 size_t			ft_strlen(const char *s);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
