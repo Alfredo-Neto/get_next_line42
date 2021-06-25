4b39c69e255e96b3f740bccdc19c612438c1b74e
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-25 15:08:48 -0300

    dup in line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 22:06:59 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/25 15:05:13 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/25 15:08:16 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static int	find_line_break(char *s_line)
 static int check_ret(char **save, char **line, ssize_t ret)
 {
 	if (ret < 0)
-		free(*line); // segfault write memory access
+		*line = ft_strdup("");
 	else
 		*line = ft_strdup(*save);
 	if (*save == NULL)
