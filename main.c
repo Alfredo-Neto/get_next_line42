820084b5993e7ed8f030274b143cb8d0d9186e53
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-25 15:25:22 -0300

    agora vai

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main(void)
         fd = open("/home/alfredobraule/gnl_test.txt", O_RDONLY);
         while (get_next_line(fd, &linha))
         {
+                // *line = NULL;
                 printf("%s", linha);
                 printf("\n");
                 free(linha);
