f489a97cb22ca9f8e21a02d40b0f1ea69e014002
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-25 15:48:25 -0300

    again

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 22:06:59 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/25 15:44:29 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/25 15:48:05 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,11 @@ static int	find_line_break(char *s_line)
 static int check_ret(char **save, char **line, ssize_t ret)
 {
 	if (ret < 0)
+	{
 		*line = ft_strdup("");
+		free(*line);
+		*line = NULL;
+	}
 	else if (*save == NULL)
 		*line = ft_strdup("");
 	else
