b6c799842ff4bd5f97deefa70977aa445f916ddf
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-25 12:47:45 -0300

    took off verification on save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 22:06:59 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/25 12:33:22 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/25 12:47:19 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,8 @@ static int	return_line(char **save, char **line, ssize_t ret)
 	if (i >= 0)
 	{
 		*line = ft_substr(*save, 0, i);
-		tmp = ft_substr(*save, i + 1, ft_strlen(*save)); // ola\njorge
-		if (*save != NULL)
-			free(*save);
+		tmp = ft_substr(*save, i + 1, ft_strlen(*save)); //ola\n\0
+		free(*save);
 		*save = tmp;
 		tmp = NULL;
 		return (1);
