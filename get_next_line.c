5113a79b58a098c60771afd93f1f8137d0267fb2
Alfredo Braule <alfredoneto934@gmail.com>
2021-06-26 17:44:13 -0300

    vai2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 22:06:59 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/06/26 17:41:25 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/06/26 17:44:05 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static int	return_line(char **save, char **line, ssize_t ret)
 	int		i;
 	char	*tmp;
 
-	if (BUFFER_SIZE <= 0)
+	if(!line || BUFFER_SIZE <= 0)
 		return (-1);
 	i = find_line_break(*save);
 	if (i >= 0)
